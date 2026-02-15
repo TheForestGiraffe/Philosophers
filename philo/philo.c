223aa0acfd42122c28599cf9c894e86087f8dc63
Pedro Lima <pedrolima.ep@gmail.com>
2026-02-15 23:42:02 +0100

    Test simple pthread implementation.

--- philo/philo.c
+++ philo/philo.c
@@ -10,17 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "philo.h"
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
-#include "philo.h"
+
+void *philo_routine(void *nbr)
+{
+	*(int *)nbr += 1;
+	printf("Philo %i\n", *(int *)nbr);
+	return (NULL);
+}
 
 int	main(int argc, char **argv)
 {
 	t_data		data;
+	int			nbr;
+	pthread_t	philo_1;
+	pthread_t	philo_2;
+	pthread_t	philo_3;
 
 	if (parse(argc, argv, &data))
 		return (1);
+	
+	nbr = 2;
+	pthread_create(&philo_1, NULL, philo_routine, (void *)&nbr);
+	pthread_create(&philo_2, NULL, philo_routine, (void *)&nbr);
+	pthread_create(&philo_3, NULL, philo_routine, (void *)&nbr);
+	
+	pthread_join(philo_1, NULL);
+	pthread_join(philo_2, NULL);
+	pthread_join(philo_3, NULL);
+
 	return (0);
 }
