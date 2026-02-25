c43783c6170ec73a264c69ecc602efde75ce68d4
pecavalc <pecavalc@student.42berlin.de>
2026-02-25 20:10:24 +0100

    Restart work: review (now called) forks.c (WIP)

---
+++ forks.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   forks.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 20:12:52 by pecavalc          #+#    #+#             */
+/*   Updated: 2026/02/25 19:54:33 by pecavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <pthread.h>
+#include <stdlib.h>
+#include "philo.h"
+
+/* forks_create owns *forks and transfers ownership on return */
+
+pthread_mutex_t	*forks_create(const long nr_philos)
+{
+	pthread_mutex_t	*forks;
+	int				i;
+
+	if (nr_philos < 1)
+		return (NULL);
+
+	forks = malloc(sizeof(pthread_mutex_t) * nr_philos);
+	if (!forks)
+		return (NULL);
+	i = 0;
+	while (i < nr_philos)
+	{
+		if (pthread_mutex_init(&(forks[i]), NULL) != 0)
+		{
+			forks_destroy(i, &forks);
+			return (NULL);
+		}
+		i++;
+	}
+	return (forks);
+}
+
+/* forks_destroy destroys and frees an array created by forks_create.
+ * Safe to call with NULL or *forks == NULL.*/
+
+void	forks_destroy(const long nr_forks_to_destroy, pthread_mutex_t **forks)
+{
+	int	i;
+
+	if (!forks || !*forks)
+		return ;
+	i = 0;
+	while (i < nr_forks_to_destroy)
+		pthread_mutex_destroy(&(*forks)[i++]);
+	free(*forks);
+	*forks = NULL;
+}
