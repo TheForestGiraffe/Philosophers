4b2a266c7c90295b391f10c5b2bf7fac6d4407c0
pecavalc <pecavalc@student.42berlin.de>
2026-02-27 13:23:38 +0100

    Start over. Move previous code to temp folder.

--- philo.h
+++ philo.h
@@ -1,17 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/03 20:54:31 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/02/27 10:13:53 by pecavalc         ###   ########.fr       */
+/*   Created: 2025/12/03 21:14:47 by pecavalc          #+#    #+#             */
+/*   Updated: 2026/02/27 13:22:38 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char **argv)
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <pthread.h>
+#include <sys/time.h>
+
+typedef struct s_fork
 {
-	
-	return (0);
-}
+	pthread_mutex_t	fork_mutex;
+	int				fork_id;
+}					t_fork;
+
+// WIP
+typedef struct s_philo
+{
+	int			id;
+	long		meals_counter;
+	bool		has_reached_max_meals;
+	long		last_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+
+}				t_philo;
+
+
+#endif
