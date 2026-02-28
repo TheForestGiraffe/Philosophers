4a28300cd2a9a76464e2205a72efe13dc0356016
pecavalc <pecavalc@student.42berlin.de>
2026-02-28 16:07:58 +0100

    Implement parser.

--- validate_and_parse_input.c
+++ validate_and_parse_input.c
@@ -1,38 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse.c                                            :+:      :+:    :+:   */
+/*   validate_and_parse_input.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 21:39:13 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/02/26 15:11:24 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/02/28 16:04:40 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "main.h"
+#include "philo.h"
 
-int	parse(int argc, char **argv, t_data *data)
+int	validate_and_parse_input(int argc, char **argv, t_app_data *app)
 {
 	if (argc < 5 || argc > 6)
 	{
-		printf("philo: invalid number of arguments\n");
+		printf("Invalid number of arguments\n");
 		return (1);
 	}
-	if (ft_strtol(argv[1], &data->nr_philos)
-		|| ft_strtol(argv[2], &data->time_to_die)
-		|| ft_strtol(argv[3], &data->time_to_eat)
-		|| ft_strtol(argv[4], &data->time_to_sleep))
+	if (ft_strtol(argv[1], &app->nbr_philos)
+		|| ft_strtol(argv[2], &app->time_to_die)
+		|| ft_strtol(argv[3], &app->time_to_eat)
+		|| ft_strtol(argv[4], &app->time_to_sleep))
 	{
-		printf("philo: one or more arguments are invalid\n");
+		printf("One or more arguments are invalid\n");
 		return (1);
 	}
 	if (argc == 5)
-		data->nr_times_each_must_eat = 0;
+			app->has_limit_nbr_meals = false;
 	if (argc == 6)
 	{
-		if (ft_strtol(argv[5], &data->nr_times_each_must_eat))
+		app->has_limit_nbr_meals = true;
+		if (ft_strtol(argv[5], &app->limit_nbr_meals))
 		{
 			printf("philo: one or more arguments are invalid\n");
 			return (1);
