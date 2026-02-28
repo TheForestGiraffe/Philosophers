b24ecaf8f03f041a0acddfcb9caeda12d2bf7f48
pecavalc <pecavalc@student.42berlin.de>
2026-02-28 16:10:26 +0100

    Add validate_and_parse_input and app_setup to main.

--- main.c
+++ main.c
@@ -6,12 +6,23 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 20:54:31 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/02/27 10:13:53 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/02/28 16:01:58 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
 int	main(int argc, char **argv)
 {
+	t_app_data	app;
+
+	if(validate_and_parse_input(argc, argv, &app))
+		return (1);
+	if(app_setup(&app)) // TODO: implement
+	{
+		app_destroy(&app); // TODO: implement
+		return (1);
+	}
 	
 	return (0);
 }
