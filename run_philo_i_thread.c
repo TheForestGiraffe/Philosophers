dbad2b0978f48ece08b8b2229c24cc06ab6754f7
pecavalc <pecavalc@student.42berlin.de>
2026-03-04 20:50:11 +0100

    WIP: implement run_philo_thread - add spinlock to wait for all threads to be ready.

--- run_philo_i_thread.c
+++ run_philo_i_thread.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 11:47:51 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/03/02 13:55:57 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/03/04 20:49:08 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,26 @@ void	*run_philo_i_thread(void *philo_i)
 	t_philo	*philo;
 
 	philo = (t_philo *)philo_i;
+	
+	// Thread start sync - spinlock: Wait for all threads to be ready
+	while (1)
+	{
+		if (pthread_mutex_lock(&philo->app->app_mutex))
+			return (NULL);
+		if (philo->app->all_threads_ready)
+		{
+			if (pthread_mutex_unlock(&philo->app->app_mutex));
+				return (NULL);
+			break;
+		}
+		if (pthread_mutex_unlock(&philo->app->app_mutex))
+			return (NULL);
+	}
 
+	// Check if simulation has ended
+
+
+	
+	
 	return (NULL); // TODO.
 }
