80185a3b49505293a0045c2cbc27ea3b91453157
Pedro Lima <pedrolima.ep@gmail.com>
2026-03-09 02:24:17 +0100

    Update philo_think

--- philo_think.c
+++ philo_think.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 20:31:48 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/03/09 00:59:53 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/03/09 02:21:46 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,8 @@ int	philo_think(t_philo *philo)
 
 	if (thread_safe_printf(IS_THINKING, philo))
 		return (1);
-	think_time_us = (((philo->app->time_to_eat * 2)
-				- philo->app->time_to_sleep) * 1000L) / 2;
-	if (think_time_us > 0)
+	think_time_us = (philo->app->time_to_eat / 2) * 1000;
+	if (philo->app->nbr_philos % 2 != 0)
 		if (feedback_based_usleep(think_time_us, philo->app))
 			return (1);
 	return (0);
