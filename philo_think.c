c5086e6a2a99a299c71ebf037575fad087557458
Pedro Lima <pedrolima.ep@gmail.com>
2026-03-09 01:31:50 +0100

    Try making system more fair and work with 5 800 200 200

--- philo_think.c
+++ philo_think.c
@@ -6,15 +6,23 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/08 20:31:48 by pecavalc          #+#    #+#             */
-/*   Updated: 2026/03/09 00:12:37 by pecavalc         ###   ########.fr       */
+/*   Updated: 2026/03/09 00:59:53 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+#include <unistd.h>
 
 int	philo_think(t_philo *philo)
 {
+	long	think_time_us;
+
 	if (thread_safe_printf(IS_THINKING, philo))
 		return (1);
+	think_time_us = (((philo->app->time_to_eat * 2)
+				- philo->app->time_to_sleep) * 1000L) / 2;
+	if (think_time_us > 0)
+		if (feedback_based_usleep(think_time_us, philo->app))
+			return (1);
 	return (0);
 }
